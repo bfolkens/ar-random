8c5753cbf3b8dc221d5dbfdc4cda819746a0ce69
Bradford Folkens <bfolkens@gmail.com>
2014-08-21 17:17:15 -0700

    Added alternate randomizing keys

--- lib/ar-random/relation.rb
+++ lib/ar-random/relation.rb
@@ -6,19 +6,37 @@ module ARRandom
     module Relation
 
       # Return a randomly selected item
-      def random
+      def random(key = primary_key)
         relation = clone
 
-        min_rec = relation.select(primary_key).first
-        max_rec = relation.order("#{primary_key} desc").select(primary_key).first
-        return relation.first unless min_rec and max_rec and min_rec != max_rec
+        min_rec = relation.order("#{key} asc").select(key).first
+        max_rec = relation.order("#{key} desc").select(key).first
+        return relation.order("#{key} asc").first unless min_rec and max_rec and min_rec != max_rec
 
-        min_id = min_rec.attributes[primary_key]
-        max_id = max_rec.attributes[primary_key]
+        min_id, clazz = encode_randomizing_value(min_rec.attributes[key.to_s])
+        max_id, clazz = encode_randomizing_value(max_rec.attributes[key.to_s])
 
         # Caveat: these must end up being rounded or we won't retrieve all records
         rand_key = (rand(max_id - min_id + 1) + min_id).round
-        relation.where("#{primary_key} >= ?", rand_key).first
+        relation.where("#{key} >= ?", decode_randomizing_value(rand_key, clazz)).order("#{key} asc").first
+      end
+
+      private
+
+      def encode_randomizing_value(value)
+        if value.kind_of? Time
+          [value.to_f, value.class]
+        else
+          [value, value.class]
+        end
+      end
+
+      def decode_randomizing_value(value, clazz)
+        if clazz.name == 'Time'
+          Time.at(value)
+        else
+          value
+        end
       end
       
     end
@@ -26,8 +44,8 @@ module ARRandom
     # Class methods
     module Base
 
-      def random
-        relation.random
+      def random(key = primary_key)
+        relation.random(key)
       end
 
     end
--- lib/ar-random/version.rb
+++ lib/ar-random/version.rb
@@ -1,3 +1,3 @@
 module ARRandom
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
